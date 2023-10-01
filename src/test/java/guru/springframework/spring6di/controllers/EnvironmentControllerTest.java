ca8b9a467a97958a8d7e684bb06e34e4078d443c
Michele COnversa <michele.conversa@gmail.com>
2023-10-01 11:32:00 +0200

    Profiles and qualifiers

--- src/test/java/guru/springframework/spring6di/controllers/EnvironmentControllerTest.java
+++ src/test/java/guru/springframework/spring6di/controllers/EnvironmentControllerTest.java
@@ -1,22 +1,22 @@
 package guru.springframework.spring6di.controllers;
 
-import guru.springframework.spring6di.services.GreetingServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//@ActiveProfiles({"dev", "EN"})
 @SpringBootTest
-class ConstructorInjectedControllerTest {
+class EnvironmentControllerTest {
 
     @Autowired
-    ConstructorInjectedController controller;
+    EnvironmentController controller;
 
     @Test
-    void sayHello() {
-        System.out.println(controller.sayHello());
+    void getEnvironment() {
+        System.out.println(controller.getEnvironment());
 
     }
 }
